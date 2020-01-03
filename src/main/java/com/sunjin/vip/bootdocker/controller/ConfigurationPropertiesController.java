011618351a1225feee9dd7de3b5601367bd808a3
sunjin991178@163.com <sunjin991178@163.com>
2020-01-03 16:15:39 +0800

    添加bus

--- src/main/java/com/sunjin/vip/bootdocker/controller/ConfigurationPropertiesController.java
+++ src/main/java/com/sunjin/vip/bootdocker/controller/ConfigurationPropertiesController.java
@@ -2,6 +2,7 @@ package com.sunjin.vip.bootdocker.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @Date: 2019/12/13
  */
 @RestController
+@RefreshScope
 public class ConfigurationPropertiesController {
 
 
